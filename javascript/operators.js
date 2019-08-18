d928aad783ece16244a776980238409785271120
Ttendo Becky <42311866+TTENDO@users.noreply.github.com>
2019-08-18 17:00:03 +0300

    adding logical operators and the modified assignments(1-3)

---
+++ javascript/operators.js
@@ -0,0 +1,69 @@
+/*console.log(3+5*4);
+console.log(3+2**4);
+console.log((3+5)*4);
+console.log(3%2);
+
+let a,b;
+a = b = 2 + 4;
+console.log('a =', a);
+console.log('b =', b);
+b = 2 * 4;
+console.log("Now b=",b);
+console.log("b % a = ",b % a);
+
+a = a**b;
+console.log("Now a=",a**b);
+
+a = 5;
+console.log("a = ",a);
+a++;
+console.log("now a =",a);
+a=a+1;
+console.log("now a =",a);
+
+a--
+console.log("now a =",a);*/
+let a = 5;
+a+=5;
+console.log("a =",a);
+a = a +5;
+console.log("a =",a);
+let b = 1; a = 1;
+let c = ++a;
+console.log("c = ",c);
+console.log("a = ",a);
+c = a++;
+/*console.log("now c =",c);
+console.log("now a=",a);*/
+ let d = b++;
+ console.log( "d = ", d);
+
+ console.log(2);
+
+ //TO LOWERCSE FUNCTION
+ name = prompt("What is your name?")
+console.log({name})
+if (name.toLowerCase() == "Becky"){
+console.log("Good day")
+}
+else if (name){
+console.log("Hello" + " " + name)
+}
+else{
+console.log("Hello!!")
+}
+//VM1607:2 {name: "BECKY"}
+//VM1607:7 Hello BECKY
+
+age = prompt("What is your age?");
+message = age < 3 ? "Hi baby" : 
+age < 18 ? "Hello": 
+age < 100 ? "Greetings" :
+ "What a strange age!!"
+ 
+
+
+
+
+
+
